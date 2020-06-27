936c5b7befa108d314a6615814c09111b935135c
rpassalacqua <rjgp1998@hotmail.com>
2020-06-27 11:40:19 +0100

    Updates

---
+++ precache-manifest.d9f9f0710b43f6ce357b320689157341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab36272dc426bd5c20d0d1f706dc3dd",
+    "url": "/clothing-store/index.html"
+  },
+  {
+    "revision": "c1d726b65b845ddfd09c",
+    "url": "/clothing-store/static/css/main.75472e43.chunk.css"
+  },
+  {
+    "revision": "eaf332fc7fbe2cfb9991",
+    "url": "/clothing-store/static/js/2.a206b372.chunk.js"
+  },
+  {
+    "revision": "c1d726b65b845ddfd09c",
+    "url": "/clothing-store/static/js/main.733fa17b.chunk.js"
+  },
+  {
+    "revision": "268624fc6c01d4499a71",
+    "url": "/clothing-store/static/js/runtime-main.ccca1819.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/clothing-store/static/media/crown.ed28accb.svg"
+  }
+]);
